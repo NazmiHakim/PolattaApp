89f538e342f06a61498a86b4f34c5862de6a4e49
NazmiHakim <145305124+NazmiHakim@users.noreply.github.com>
2025-06-12 18:58:48 +0800

    landing page update

--- app/src/main/java/com/example/pondokcokelathatta/ui/components/MenuList.kt
+++ app/src/main/java/com/example/pondokcokelathatta/ui/components/MenuList.kt
@@ -1,7 +1,18 @@
 package com.example.pondokcokelathatta.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -15,7 +26,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -49,7 +60,7 @@ fun MenuList(menuItems: List<MenuItem>) {
 
 @Composable
 fun MenuCard(item: MenuItem) {
-    var quantity by remember { mutableStateOf(0) }
+    var quantity by remember { mutableIntStateOf(0) }
 
     Box(
         modifier = Modifier
@@ -162,7 +173,7 @@ fun MenuCard(item: MenuItem) {
 
                 // Konten Bawah: Harga
                 Text(
-                    text = "${"%,d".format(item.price).replace(',', '.')}",
+                    text = "%,d".format(item.price).replace(',', '.'),
                     fontWeight = FontWeight.Bold,
                     color = TextPrimary,
                     fontSize = 18.sp,
